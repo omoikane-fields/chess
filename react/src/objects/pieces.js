9089fe0f0dcff0309340104341cb371ed6fdfa5c
annonymous <rduncan@DESKTOP-RJ3TS35>
2026-08-22 14:09:35 -0600

    add moves to all pieces.

--- react/src/objects/pieces.js
+++ react/src/objects/pieces.js
@@ -85,7 +85,82 @@ function pawnCaptures({ board, piece, from, direction }) {
     .map((to) => move(from, to, "capture"));
 }
 
+/*
+ * Rook Movement Logic
+ */
+// will work for queen and bishop as well, just need to change the directions
+function followDirection({ board, piece, from, direction, limit = Infinity }) {
+  const moves = [];
+  let to = { x: from.x + direction.x, y: from.y + direction.y };
+
+  while (isInsideBoard(to) && moves.length < limit) {
+    if (isEmpty(board, to)) {
+      moves.push(move(from, to));
+    } else if (isEnemy(board, piece, to)) {
+      moves.push(move(from, to, "capture"));
+      break; // stop after capturing an enemy piece
+    } else {
+      break; // stop if there's a friendly piece
+    }
+
+    to = { x: to.x + direction.x, y: to.y + direction.y };
+  }
+
+  return moves;
+}
+
+function orthogonalRay({ board, piece, from, limit }) {
+  const directions = [
+    { x: 1, y: 0 }, // right
+    { x: -1, y: 0 }, // left
+    { x: 0, y: 1 }, // down
+    { x: 0, y: -1 }, // up
+  ];
+
+  return directions.flatMap((direction) =>
+    followDirection({ board, piece, from, direction, limit }),
+  );
+}
+
+function diagonalRay({ board, piece, from, limit }) {
+  const directions = [
+    { x: 1, y: 1 }, // down-right
+    { x: -1, y: 1 }, // down-left
+    { x: 1, y: -1 }, // up-right
+    { x: -1, y: -1 }, // up-left
+  ];
+
+  return directions.flatMap((direction) =>
+    followDirection({ board, piece, from, direction, limit }),
+  );
+}
+
+function lShapeMoves({ board, piece, from }) {
+  const offsets = [
+    { x: 1, y: 2 },
+    { x: 1, y: -2 },
+    { x: -1, y: 2 },
+    { x: -1, y: -2 },
+    { x: 2, y: 1 },
+    { x: 2, y: -1 },
+    { x: -2, y: 1 },
+    { x: -2, y: -1 },
+  ];
+
+  return offsets
+    .map((offset) => ({ x: from.x + offset.x, y: from.y + offset.y }))
+    .filter((to) => isInsideBoard(to))
+    .filter((to) => isEmpty(board, to) || isEnemy(board, piece, to))
+    .map((to) => move(from, to));
+}
+
+const bishopMoves = composeMoves(diagonalRay);
+const queenMoves = composeMoves(orthogonalRay, diagonalRay);
 const pawnMoves = composeMoves(pawnSingleStep, pawnDoubleStep, pawnCaptures);
+const rookMoves = composeMoves(orthogonalRay);
+const knightMoves = composeMoves(lShapeMoves);
+const kingMoves = (context) =>
+  composeMoves(orthogonalRay, diagonalRay)({ ...context, limit: 1 });
 
 export function getCandidateMoves(board, piece, from, gameState) {
   const direction = piece.color === "white" ? -1 : 1;
@@ -101,4 +176,9 @@ export function getCandidateMoves(board, piece, from, gameState) {
 
 const moveGenerators = {
   pawn: pawnMoves,
+  rook: rookMoves,
+  bishop: bishopMoves,
+  queen: queenMoves,
+  knight: knightMoves,
+  king: kingMoves,
 };
